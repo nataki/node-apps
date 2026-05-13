ff8858bb5c1663eec9750079d53bcb849b30a97e
nataki <nat.aki.nat.aki@gmail.com>
2026-05-13 22:19:41 +0200

    Add auth route (form submit)

--- express/app.js
+++ express/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const authRouter = require('./routes/auth');
+
 const app = express();
 const port = 5001;
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1>');
-});
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use('/login', authRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
