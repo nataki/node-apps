d63b256825151acf4a93b20a470f6add245b1a54
nataki <nat.aki.nat.aki@gmail.com>
2026-05-13 22:10:51 +0200

    Add express app

---
+++ express/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5001;
+app.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
